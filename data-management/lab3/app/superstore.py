cd100b891cb1a2c05117ecddab4d5736eefd4b6d
James <hoyinchui5@gmail.com>
2024-10-14 21:50:23 -0400

    added back the old version code to avoid conflit

--- data-management/lab3/app/superstore.py
+++ data-management/lab3/app/superstore.py
@@ -698,6 +698,86 @@ GROUP BY s.Ship_Mode;
 df_base = pd.read_sql(queryExec, connection)
 df_base.to_excel(outdir + 'executive.xlsx', index=False, engine='openpyxl')
 
+# this part is the old version of the code
+queryExec = """
+select ship_mode as 'Shipping Mode',
+ round(sales_q4_20) as 'Sales Q4-20',
+ round(sales_q4_21) as 'Sales Q4-21',
+ round(((sales_q4_21 - sales_q4_20)/sales_q4_20)* 100) as 'Q4 Sales YOY',
+ round(avg_ship_time_q4_21) as 'Average Shipping Time',
+ round((profit_q4_21/sales_q4_21)*100) as 'Profit Margin',
+ round((return_q4_21/total_orders)*100) as 'Return Rate',
+ round((discount_mean*100)/profit_mean) 'Discount Profit Ratio'
+ from (
+ select s.ship_mode,
+        SUM(CASE 
+        WHEN o.order_date BETWEEN '2021-10-01' AND '2021-12-31' 
+        THEN sale.sales 
+        ELSE 0 
+    END) AS sales_q4_21,
+        SUM(CASE 
+        WHEN o.order_date BETWEEN '2021-10-01' AND '2021-12-31' 
+        THEN sale.profit 
+        ELSE 0 
+    END) AS profit_q4_21,
+    SUM(CASE 
+        WHEN o.order_date BETWEEN '2020-10-01' AND '2020-12-31' 
+        THEN sale.sales 
+        ELSE 0 
+    END) AS sales_q4_20,
+    SUM(CASE 
+        WHEN o.order_date BETWEEN '2021-07-01' AND '2021-09-30'  
+        THEN sale.sales 
+        ELSE 0 
+    END) AS sales_q3_21,
+     avg(CASE 
+        WHEN o.order_date BETWEEN '2021-10-01' AND '2021-12-31' 
+        THEN DATEDIFF(s.ship_date, o.order_date)
+        ELSE null 
+    END) AS avg_ship_time_q4_21,
+     SUM(CASE 
+        WHEN o.order_date BETWEEN '2021-10-01' AND '2021-12-31' and o.returned = 1 
+        THEN 1 
+        ELSE 0 
+    END) AS return_q4_21,
+    sum(
+    CASE 
+        WHEN o.order_date BETWEEN '2021-10-01' AND '2021-12-31'
+        THEN 1
+        ELSE 0 
+    END
+     ) as total_orders,
+     avg(
+    CASE 
+        WHEN o.order_date BETWEEN '2021-10-01' AND '2021-12-31'
+        THEN sale.discount
+        ELSE Null
+    END
+     ) as discount_mean,
+     avg(
+    CASE 
+        WHEN o.order_date BETWEEN '2021-10-01' AND '2021-12-31'
+        THEN sale.profit
+        ELSE Null
+    END
+     ) as profit_mean
+  from order_items as oi
+	left join sales as sale on oi.Order_ID=sale.Order_ID and oi.Product_KEY_ID=sale.Product_KEY_ID
+	left join orders as o on o.Order_ID=oi.Order_ID
+	left join products as p on p.Product_KEY_ID=oi.Product_KEY_ID
+	left join subcategory as sub on sub.Sub_Category_ID=p.Sub_Category_ID
+	left join category as cat on cat.Category_ID=sub.Category_ID
+	left join customers as c on o.Customer_ID=c.Customer_ID
+	left join ship as s on s.Ship_ID = o.Ship_ID
+	left join address as a on a.Address_ID=o.Address_ID
+	left join manager as m on m.Region_ID=o.Region_ID
+ group by s.ship_mode ) sales_q
+ """
+
+#df_base = pd.read_sql(queryExec, connection)
+#df_base.to_excel(outdir + 'executive.xlsx', index=False, engine='openpyxl')
+
+
 queryExec = """
 SELECT 
     'Totals/Averages' AS 'Shipping Mode',
